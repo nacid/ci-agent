d00187257103df0a00f3c107bf5ced81f4ce3886
Nikolay Bondarev <i@nacid.me>
2026-03-11 06:20:27 +0400

    Get-CimInstance fix

--- assets/profile.ps1
+++ assets/profile.ps1
@@ -1,5 +1,19 @@
 $machineName = [System.Environment]::MachineName
-$osName = (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
+$runtimeOsDescription = [System.Runtime.InteropServices.RuntimeInformation]::OSDescription
+$osName = $runtimeOsDescription
+
+if ($IsWindows) {
+    $getCimInstanceCommand = Get-Command -Name 'Get-CimInstance' -ErrorAction SilentlyContinue
+    if ($getCimInstanceCommand) {
+        try {
+            $cimOsName = (Get-CimInstance -ClassName Win32_OperatingSystem -ErrorAction Stop).Caption
+            if (-not [string]::IsNullOrWhiteSpace($cimOsName)) {
+                $osName = $cimOsName
+            }
+        } catch {
+        }
+    }
+}
 
 Write-Host "$machineName ($osName)"
 
